d8af5ab3ff6b2d19e91a9e89683fa72d99214669
nv-webblog68 <chalermchaib66@nu.ac.th>
2025-12-25 16:31:41 +0700

    kk

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,49 @@
-let express = require('express')
-let bodyParser = require('body-parser')
- 
-const app = express()
- 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
- 
-// เรียกใช้ routes โดยส่ง app เข้าไป
-require('./routes')(app)
- 
-app.get('/status', function (req, res) {
-    res.send('Hello nodejs server')
+let express = require('express');
+const app = express();
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+app.get('/status', (req, res) => {
+    res.send('Hello Node.js Server!')
+});
+
+app.get('/hello/:name', function (req, res) {
+    console.log('hello - ' + req.params.name)
+    res.send('say hello with ' + req.params.name)
+});
+
+// get user by id
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน'+ req.params.userId)
 })
- 
-app.get('/hello/:person', function (req, res) {
-    console.log('hello - ' + req.params.person)
-    res.send('sey hello with ' + req.params.person)
+// get all user
+app.get('/menus', function (req, res) {
+    res.send('เรียกข้อมูลผู้ใช้งาน')
 })
- 
-let port = 8081
- 
-app.listen(port, function () {
-    console.log('server running on ' + port)
+// get all user
+app.get('/users', function (req, res) {
+    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
 })
+
+// create user
+app.post('/user/', function (req, res) {
+    res.send('ทำการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body))
+})
+
+// edit user
+app.put('/user/:userId', function (req, res) {
+    res.send('ทำการแก้ไขผู้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body))
+})
+
+// delete user
+app.delete('/user/:userId', function (req, res) {
+    res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body))
+})
+
+
+let port = process.env.PORT || 8081;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
