4d6db0cb73575fbbba313d9ad9d74d194cdfd78e
nv-webblog68 <chalermchaib66@nu.ac.th>
2026-01-22 16:53:56 +0700

    save web

--- client/src/components/Users/Index.vue
+++ client/src/components/Users/Index.vue
@@ -1,19 +1,33 @@
 <template>
   <div>
     <h1>Get All Users</h1>
+    <div v-if="users.length">
+      <div>จำนวนผู้ใช้งาน {{ users.length }}</div>
+      <div v-for="user in users" v-bind:key="user.id">
+            <p>id: {{ user.id }}</p>
+            <p>ชื่อ-นามสกุล: {{ user.name }} - {{ user.lastname }}</p>
+            <p>email: {{ user.email }}</p>
+            <p>password: {{ user.password }}</p>
+            <hr>
+            <p><button v-on:click="navigateTo('/user/'+user.id)">ดูข้อมูลผู้ใช้</button></p>
+        </div>
+
+    </div>
+
     <p><button @click="navigateTo('/user/create')">สร้างผู้ใช้งาน</button></p>
   </div>
 </template>
 
 <script>
-import axios from "axios"; // Import axios เข้ามาใช้งานโดยตรง
+import UsersService from '../../services/UsersService'
 
 export default {
   data() {
     return {
-      users: [],
-    };
+      users: []
+    }
   },
+
 
   // Logic จะเขียนตรงนี้
   methods: {
@@ -22,12 +36,16 @@ export default {
     },
   },
 
-  async created(){
-    const response = await axios.get("http://localhost:8081/users")
-    this.users = response.data;
-    console.log("Fetched users:", this.users);
+  async created() {
+    try {
+      this.users = (await UsersService.index()).data
+      console.log(this.users)
+    } catch (error) {
+      console.log(error)
+    }
   }
-};
+}
+
 </script>
 
 <style scoped>
